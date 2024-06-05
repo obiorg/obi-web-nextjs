32d0a72b43c6e52640344d44125d4401882e71f6
kfoxone <raphaelhendrick@gmail.com>
2024-06-06 00:37:07 +0100

    Restore working project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,23 @@
 'use client';
-<<<<<<<< HEAD:app/layout.tsx
-import { LayoutProvider } from '../layout/context/layoutcontext';
-========
+
+
 import { LayoutContext, LayoutProvider } from '@/layout/context/layoutcontext';
->>>>>>>> parent of 7e662ea (Revert "Wrong lang"):app/[lang]/layout.tsx
+
 import { PrimeReactProvider } from 'primereact/api';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
+import { useContext } from 'react';
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-<<<<<<<< HEAD:app/layout.tsx
-========
     
     const { layoutConfig } = useContext(LayoutContext);
-    console.log('Starting root layout')
->>>>>>>> parent of 7e662ea (Revert "Wrong lang"):app/[lang]/layout.tsx
     return (
         <html lang="en" suppressHydrationWarning>
             <head>

077846d8e80c58add1ab16610f831191ddcf7fa4
Hendrick <19176533+kfoxone@users.noreply.github.com>
2024-10-04 21:53:15 +0100

    Add a DialogError in place of page error to manage error handling

--- src/obi/components/Dialog/DialogError.tsx
+++ src/obi/components/Dialog/DialogError.tsx
@@ -12,6 +12,7 @@ import { Menu } from "primereact/menu";
 import { MultiSelect } from "primereact/multiselect";
 import { OverlayPanel } from "primereact/overlaypanel";
 import { Panel } from "primereact/panel";
+import { ScrollPanel } from "primereact/scrollpanel";
 import { SelectButton } from "primereact/selectbutton";
 import { Toast } from "primereact/toast";
 import { Toolbar } from "primereact/toolbar";
@@ -80,18 +81,52 @@ export default function DialogError({
                 onHide={() => setDialogErr(false)}
                 maximizable modal
             >
-                <p>
-                    <span className='p-text-bold'>{error?.message}</span>
-                </p>
-                <Panel header="Plus d'infos" toggleable collapsed={true} >
-                    <p>
-                        {error?.error?.stack}
-                    </p>
-                </Panel>
 
-                <p><br />
-                    Corriger... ! Voulez-vous actualiser ?
-                </p>
+
+
+
+                <div className="flex flex-column justify-content-center">
+                    <div className="flex flex-column align-items-center">
+                        <img src="/layout/images/obi/obi-signet-danger.svg"
+                            alt="obi-signet-danger svg"
+                            className="mb-3 w-6rem flex-shrink-0 align-items-center" />
+                    </div>
+                    <div
+                        style={{
+                            borderRadius: '56px',
+                            padding: '0.3rem',
+                            background: 'linear-gradient(180deg, rgba(233, 30, 99, 0.4) 10%, rgba(33, 150, 243, 0) 30%)'
+                        }}
+                    >
+                        <div className=" surface-card py-6 px-5 sm:px-8 flex flex-column"
+                            style={{ borderRadius: '53px' }}>
+
+                            <div className="flex flex-column align-items-center">
+                                <div className="flex flex-column justify-content-center align-items-center  bg-pink-500 border-circle "
+                                    style={{ height: '3.2rem', width: '3.2rem' }}>
+                                    <i className="pi pi-fw pi-exclamation-circle text-2xl text-white"></i>
+                                </div>
+                            </div>
+
+                            <h5>{error?.message}</h5>
+                            <Panel 
+                                header="Plus d'infos" 
+                                toggleable 
+                                collapsed={false} 
+                                className="flex-column align-items-start ml-0 pl-0"
+                                 >
+                                <ScrollPanel className="m-0" style={{ width: '100%', height: '60px' }} >
+                                    <p>{error?.error?.stack}</p>
+                                </ScrollPanel>
+                            </Panel>
+
+                            <div className="flex flex-column align-items-center justify-content-between p-0 ">
+                                <div className="text-600 mt-3 mb-5">Voulez-vous actualiser ?</div>
+                                <img src="/demo/images/error/asset-error.svg" alt="Error" className="mb-2" width="80%" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Dialog>
 
 
